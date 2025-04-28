c0d58eea50cabb9f9824b5807294aebca33d4bea
Alex Foster <alex.88.foster@gmail.com>
2025-04-29 01:09:31 +1000

    Draw basic ground tiles

--- player/Player.cs
+++ player/Player.cs
@@ -4,53 +4,53 @@ using Godot;
 
 public partial class Player : CharacterBody2D
 {
-    // Speed of the player
-    [Export]
-    public float Speed = 200.0f;
+	// Speed of the player
+	[Export]
+	public float Speed = 200.0f;
 
-    [Export]
-    public AnimatedSprite2D animatedSprite2D;
+	[Export]
+	public AnimatedSprite2D animatedSprite2D;
 
-    public override void _PhysicsProcess(double delta)
-    {
-        Vector2 velocity = Vector2.Zero; // The player's movement vector
-        String direction = PlayerAnimatedSprite2d.DEFAULT;
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Vector2.Zero; // The player's movement vector
+		String direction = PlayerAnimatedSprite2d.DEFAULT;
 
-        // Get input direction
-        if (Input.IsActionPressed("ui_right"))
-        {
-            velocity.X += 1;
-            direction = PlayerAnimatedSprite2d.WALK_EAST;
-        }
-        if (Input.IsActionPressed("ui_left"))
-        {
-            velocity.X -= 1;
-            direction = PlayerAnimatedSprite2d.WALK_WEST;
+		// Get input direction
+		if (Input.IsActionPressed("ui_right"))
+		{
+			velocity.X += 1;
+			direction = PlayerAnimatedSprite2d.WALK_EAST;
+		}
+		if (Input.IsActionPressed("ui_left"))
+		{
+			velocity.X -= 1;
+			direction = PlayerAnimatedSprite2d.WALK_WEST;
 
-        }
-        if (Input.IsActionPressed("ui_down"))
-        {
-            velocity.Y += 1;
-            direction = PlayerAnimatedSprite2d.WALK_SOUTH;
+		}
+		if (Input.IsActionPressed("ui_down"))
+		{
+			velocity.Y += 1;
+			direction = PlayerAnimatedSprite2d.WALK_SOUTH;
 
-        }
-        if (Input.IsActionPressed("ui_up"))
-        {
-            velocity.Y -= 1;
-            direction = PlayerAnimatedSprite2d.WALK_NORTH;
+		}
+		if (Input.IsActionPressed("ui_up"))
+		{
+			velocity.Y -= 1;
+			direction = PlayerAnimatedSprite2d.WALK_NORTH;
 
-        }
+		}
 
-        animatedSprite2D.Play(direction);
+		animatedSprite2D.Play(direction);
 
-        // Normalize the velocity to ensure consistent speed in all directions
-        if (velocity.Length() > 0)
-        {
-            velocity = velocity.Normalized() * Speed;
-        }
+		// Normalize the velocity to ensure consistent speed in all directions
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+		}
 
-        // Move the player
-        Velocity = velocity;
-        MoveAndSlide();
-    }
+		// Move the player
+		Velocity = velocity;
+		MoveAndSlide();
+	}
 }
--- player/PlayerAnimatedSprite2d.cs
+++ player/PlayerAnimatedSprite2d.cs
@@ -3,10 +3,10 @@ using System;
 
 public partial class PlayerAnimatedSprite2d : AnimatedSprite2D
 {
-    public const string DEFAULT = "default";
-    public const string WALK_NORTH = "walk_north";
-    public const string WALK_SOUTH = "walk_south";
-    public const string WALK_EAST = "walk_east";
-    public const string WALK_WEST = "walk_west";
+	public const string DEFAULT = "default";
+	public const string WALK_NORTH = "walk_north";
+	public const string WALK_SOUTH = "walk_south";
+	public const string WALK_EAST = "walk_east";
+	public const string WALK_WEST = "walk_west";
 
 }
